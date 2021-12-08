4a2f494fca46fdf8f13bcb0ce36ffee7ed29c29e
datastructblues <86204793+datastructblues@users.noreply.github.com>
2021-12-08 03:10:00 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # number-guessing
-Number guessing game that will record the high scores in "highscores.dat" file.  You must find the random number (it's given by program) at least with 10 attempt.  This program written by java.
+Number guessing game that will record the high scores in "highscores.dat" file. 
+You must find the random number (it's given by program) at least with 10 attempt. 
+This program written by java.
---
+++ src/GuessNumber.java
@@ -0,0 +1,94 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+public class GuessNumber {
+    static Integer point=0;
+    static Integer count = 0;
+    public static void main(String[] args) {
+        String name =getPlayerName();
+        startGame();
+        writeFile(name,point);
+
+
+    }
+    public static String compareGuess(int userAnswer, int computerAnswer, int ct) {
+        if (userAnswer == computerAnswer) {
+            return "Correct!\n Attempt number: " + ct + "\n Your point: " + calculatePoint(point, count);
+        } else if (userAnswer <= 0 || userAnswer > 100) {
+            return "Invalid range";
+        } else if (userAnswer > computerAnswer) {
+            return "Too high, try again \n Attempt number: " + ct;
+        } else if (userAnswer < computerAnswer) {
+            return "Too low, try again:\n Attempt number: " + ct;
+        } else {
+            return "Incorrect entry.Attempt number: " + ct;
+        }
+    }
+
+    public static void startGame() {
+        int computerNumber = (int) (Math.random() * 100 + 1);
+        int userAnswer = 0;
+        while (userAnswer!=computerNumber) {
+            if(++count>=10) {
+                JOptionPane.showMessageDialog(null, "Game over... \n Correct number is: "+computerNumber);
+                break;
+            }
+            String response = JOptionPane.showInputDialog(null,
+                    "Enter a number between 1 and 100");
+
+            try {
+                userAnswer = Integer.parseInt(response);
+
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Number expected.");
+            }
+            JOptionPane.showMessageDialog(null, "" + compareGuess(userAnswer, computerNumber, count));
+
+        }
+        point=calculatePoint(point,count);
+    }
+    public static int calculatePoint(int point,int count){
+
+        return point=((10-count)*100)+100;
+    }
+    public static int getPoint(){
+        return point;
+    }
+
+    public static String getPlayerName(){
+        String player_name=JOptionPane.showInputDialog(null,"Enter player name: ");
+        return player_name;
+    }
+
+    public static void writeFile(String name, Integer point){
+        FileOutputStream file = null;
+            try {
+                file=new FileOutputStream("highscores.dat",true);
+                String score=(name+":"+ point);
+                byte[] array=score.getBytes(StandardCharsets.UTF_8);
+                try{
+                   file.write(array);
+                   file.flush();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException ex){
+                    ex.printStackTrace();
+                }finally {
+                try {
+                    file.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+}
+
+
+
